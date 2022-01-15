7b9694e5f186bb7bf1791e7268aaccee511873dc
megumi123 <megumikei@qq.com>
2022-01-15 13:42:03 +0800

    test git push

---
+++ BMS/IDLE/testPullPush.cpp
@@ -0,0 +1,13 @@
+/*
+ * @Author: Kinoko 
+ * @Date: 2022-01-15 13:41:09 
+ * @Last Modified by:   Kinoko 
+ * @Last Modified time: 2022-01-15 13:41:09 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world");
+    return 0;
+}
