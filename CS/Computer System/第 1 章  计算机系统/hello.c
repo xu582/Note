37769b408820c010ddde8a0222b592db6db4ac93
megumi123 <megumikei@qq.com>
2022-03-27 14:45:11 +0800

    测试体检hello.c生产的sinp

---
+++ CS/Computer System/第 1 章  计算机系统/hello.c
@@ -0,0 +1,12 @@
+/*
+ * @Author: Kinoko
+ * @Date: 2022-03-27 14:42:00
+ * @Last Modified by: Kinoko
+ * @Last Modified time: 2022-03-27 14:44:17
+ */
+#include <stdio.h>
+
+int main() {
+  printf("hello, world\n");
+  return 0;
+}
